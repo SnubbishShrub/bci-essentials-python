f848ec4854bdf464f9a823964abf6bbd3972fc9c
Daniel Comaduran <dcomadur@ucalgary.ca>
2025-04-02 12:59:30 -0600

    Checkpoint 2

--- bci_essentials/classification/ssvep_fbcca_classifier.py
+++ bci_essentials/classification/ssvep_fbcca_classifier.py
@@ -16,8 +16,14 @@ from ..signal_processing import (
     implement_filter_bank,
     ssvep_templates
 )
+from .generic_classifier import GenericClassifier, Prediction
+from ..utils.logger import Logger  # Logger wrapper
 
-class SSVEPFBCCClassifier:
+# Instantiate a logger for the module at the default level of logging.INFO
+# Logs to bci_essentials.__module__) where __module__ is the name of the module
+logger = Logger(name=__name__)
+
+class SSVEPFBCCClassifier(GenericClassifier):
     """SSVEP FBCCA Classifier class.
 
     This class implements the SSVEP FBCCA classifier, which uses
@@ -80,3 +86,53 @@ class SSVEPFBCCClassifier:
         self.filter_bank = filter_bank.create_filter_bank(
             target_freqs, sampling_freq, n_harmonics, n_filters
         )
+
+    def fit(self):
+        """Fit the model.
+
+        This method is not used in the FBCCA classifier, as it does not require
+        training.
+
+        Returns
+        -------
+        `None`
+            Models created used in `predict()`.
+
+        """
+        pass
+
+    def predict(self, X):
+        """Predict the class labels for the provided data.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            3D array where shape = (trials, channels, samples)
+
+        Returns
+        -------
+        prediction : Prediction
+            Results of predict call containing the predicted class labels.  Probabilities
+            are not available (empty list).
+
+        """
+        # Get the shape of the input data
+        n_trials, n_channels, n_samples = X.shape
+
+        # Initialize the predictions array
+        probabilities = np.zeros((n_trials, len(self.target_freqs)))
+
+        # Apply filter bank to each trial
+        for i in range(n_trials):
+            filtered_data = implement_filter_bank(X[i], self.filter_bank)
+
+            # Perform CCA for each target frequency
+            for j in range(len(self.target_freqs)):
+                self.cca.fit(filtered_data, self.templates[j])
+                probabilities[i, j] = self.cca.score(filtered_data, self.templates[j])
+
+        # Get the predicted class labels
+        predicted_labels = np.argmax(probabilities, axis=1)
+
+        # Create and return a Prediction object
+        return Prediction(predicted_labels, probabilities)
