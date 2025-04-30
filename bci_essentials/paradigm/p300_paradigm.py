912373674b97b5e3469fd2e551d39ae7b0bcbdb6
daniellarbourque <daniellarosebourque@gmail.com>
2025-04-30 09:08:59 -0600

    Reformatted with black

--- bci_essentials/paradigm/p300_paradigm.py
+++ bci_essentials/paradigm/p300_paradigm.py
@@ -136,7 +136,9 @@ class P300Paradigm(Paradigm):
             marker_eeg_timestamps = eeg_timestamps - marker_timestamp
 
             # Create the epoch time vector using the preprocessing window
-            epoch_time = np.arange(-self.preprocessing_window, self.preprocessing_window, 1 / fsample)
+            epoch_time = np.arange(
+                -self.preprocessing_window, self.preprocessing_window, 1 / fsample
+            )
 
             epoch_X = np.zeros((1, n_channels, len(epoch_time)))
 
@@ -157,7 +159,11 @@ class P300Paradigm(Paradigm):
             )
 
             # Trim the preprocessed epoch
-            trimmed_epoch_X = epoch_X[:, :, ((epoch_time >= self.epoch_start) & (epoch_time <= self.epoch_end))]
+            trimmed_epoch_X = epoch_X[
+                :,
+                :,
+                ((epoch_time >= self.epoch_start) & (epoch_time <= self.epoch_end)),
+            ]
 
             # For each flash index in the marker
             for flash_index in flash_indices:
@@ -171,7 +177,9 @@ class P300Paradigm(Paradigm):
                     flash_counts[flash_index] += 1
 
         # Average all epochs for each object
-        object_epochs_mean = [np.zeros((n_channels, trimmed_epoch_X.shape[-1]))] * num_objects
+        object_epochs_mean = [
+            np.zeros((n_channels, trimmed_epoch_X.shape[-1]))
+        ] * num_objects
         for i in range(num_objects):
             object_epochs_mean[i] = np.mean(object_epochs[i], axis=0)
 
