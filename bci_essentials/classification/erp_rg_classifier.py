bb4b6aeba4800c30d5991a246f0243e0d19f1602
Brian Irvine <33841739+brianjohannes@users.noreply.github.com>
2025-08-07 11:51:54 -0600

    Update erp_rg_classifier.py

--- bci_essentials/classification/erp_rg_classifier.py
+++ bci_essentials/classification/erp_rg_classifier.py
@@ -148,7 +148,7 @@ class ErpRgClassifier(GenericClassifier):
         )
 
         # Init predictions to all false
-        preds = np.zeros(len(self.y))
+        cv_preds = np.zeros(len(self.y))
 
         def __erp_rg_kernel(X, y):
             """ERP RG kernel.
@@ -173,8 +173,8 @@ class ErpRgClassifier(GenericClassifier):
                 KernelResults object containing the following attributes:
                     model : classifier
                         The trained classification model.
-                    preds : numpy.ndarray
-                        The predictions from the model.
+                    cv_preds : numpy.ndarray
+                        The predictions from the model using cross validation.
                         1D array with the same shape as `y`.
                     accuracy : float
                         The accuracy of the trained classification model.
@@ -267,7 +267,7 @@ class ErpRgClassifier(GenericClassifier):
                     y_train = y_train[remaining_ind]
 
                 self.clf.fit(X_train, y_train)
-                preds[test_idx] = self.clf.predict(X_test)
+                cv_preds[test_idx] = self.clf.predict(X_test)
                 predproba = self.clf.predict_proba(X_test)
 
                 # Use pred proba to show what would be predicted
@@ -286,12 +286,11 @@ class ErpRgClassifier(GenericClassifier):
             self.clf.fit(X, y)
             model = self.clf
 
-            training_preds = self.clf.predict(X)
-            accuracy = sum(training_preds == self.y) / len(training_preds)
-            precision = precision_score(self.y, training_preds)
-            recall = recall_score(self.y, training_preds)
+            accuracy = sum(cv_preds == self.y) / len(cv_preds)
+            precision = precision_score(self.y, cv_preds)
+            recall = recall_score(self.y, cv_preds)
 
-            return KernelResults(model, training_preds, accuracy, precision, recall)
+            return KernelResults(model, cv_preds, accuracy, precision, recall)
 
         # Check if channel selection is true
         if self.channel_selection_setup:
