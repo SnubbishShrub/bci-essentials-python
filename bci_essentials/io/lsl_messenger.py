a6100b333518d10c76319069df1f5d4d80c7170d
Twig <git@twig.gay>
2026-02-19 13:54:42 -0700

    Overhauled prediction messages - added methods to create and send prediction messages in new format, including probabilities

--- bci_essentials/io/lsl_messenger.py
+++ bci_essentials/io/lsl_messenger.py
@@ -1,6 +1,7 @@
 from mne_lsl.lsl import StreamInfo, StreamOutlet
 from .messenger import Messenger
 from ..classification.generic_classifier import Prediction
+import numpy as np
 
 __all__ = ["LslMessenger"]
 
@@ -33,5 +34,30 @@ class LslMessenger(Messenger):
         self.__outlet.push_sample(["marker received : {}".format(marker)])
 
     def prediction(self, prediction: Prediction):
-        # probability isn't used by Unity at this time
-        self.__outlet.push_sample(["{}".format(prediction.labels)])
+        prediction_message = self.format_prediction_message(prediction)
+        self.__outlet.push_sample([prediction_message])
+
+    def format_prediction_message(self, prediction: Prediction) -> str:
+        prediction_message = "%s:%s"
+        labels = prediction.labels
+        probabilities = prediction.probabilities
+
+        if len(labels) > 1:
+            probabilities = [np.average(x) for x in np.matrix_transpose(probabilities)]
+            labels = [int(np.argmax(probabilities))]
+
+        label_string = str(labels[0])
+        probabilities_string = self.format_probabilities_string(probabilities)
+
+        return prediction_message % (label_string, probabilities_string)
+
+    def format_probabilities_string(self, probabilities: list, precision: int = 4) -> str:
+        format_string = "%.{}f".format(precision)
+
+        if type(probabilities[0]) is np.ndarray:
+            probabilities = probabilities[0]
+
+        if len(probabilities) == 1:
+            return format_string % probabilities[0]
+        else:
+            return "[%s]" % " ".join([format_string % p for p in probabilities])
